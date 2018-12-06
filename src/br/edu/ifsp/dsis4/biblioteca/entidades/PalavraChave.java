c1b4968c3f1ab63f0b7b9d0b27f18216b1eda57a
drfigueira <36739215+drfigueira@users.noreply.github.com>
2018-12-06 15:11:26 -0200

    Add files via upload

--- src/br/edu/ifsp/dsis4/biblioteca/entidades/PalavraChave.java
+++ src/br/edu/ifsp/dsis4/biblioteca/entidades/PalavraChave.java
@@ -4,30 +4,30 @@ public class PalavraChave {
 
     private int id;
 
-    private String descricao;
+    private String palavra;
 
     public PalavraChave() {
 
     }
 
-    public PalavraChave (final int id, final String descricao) {
+    public PalavraChave(int id, String palavra) {
         this.id = id;
-        this.descricao = descricao;
+        this.palavra = palavra;
     }
 
-    public int getId () {
+    public int getId() {
         return id;
     }
 
-    public void setId (final int id) {
+    public void setId(int id) {
         this.id = id;
     }
 
-    public String getDescricao () {
-        return descricao;
+    public String getPalavra() {
+        return palavra;
     }
 
-    public void setDescricao (final String descricao) {
-        this.descricao = descricao;
+    public void setPalavra(String palavra) {
+        this.palavra = palavra;
     }
 }
